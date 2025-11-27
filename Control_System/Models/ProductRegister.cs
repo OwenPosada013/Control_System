7e6ba2ef2cfa3b989691eadf5b288e92aaa08a1e
AndresM0112 <andresmurillo0112@gmail.com>
2025-11-26 22:16:54 -0500

    Agregar Validaciones en el modelo de productRegister

--- Control_System/Models/ProductRegister.cs
+++ Control_System/Models/ProductRegister.cs
@@ -1,12 +1,26 @@
-﻿namespace Control_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Control_System.Models
 {
     public class ProductRegister
     {
         public int IdProductRegister { get; set; }
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        [StringLength(100, MinimumLength = 3)]
         public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, 999999.99)]
         public decimal Price { get; set; }
+        
+        [Required]
+        [Range(1, 10000)]
         public int Cant { get; set; }
         public string? TypeProduct { get; set; }
+
+        [Required(ErrorMessage = "El proveedor es obligatorio")]
+        [StringLength(100)]
         public string Provider { get; set; }
     }
 }
